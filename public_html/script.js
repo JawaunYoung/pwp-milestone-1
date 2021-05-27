78f47a39b5aff48620b33a11f382130f18ecff8b
Jawaun Young <jyoung84@cnm.edu>
2021-05-27 06:23:30 -0700

    adding jquery form

---
+++ public_html/script.js
@@ -0,0 +1,35 @@
+$(document).ready(
+    $("#contact").validate({
+        debug:true,
+        errorErrorClass: "alert alert-danger",
+        errorLabelContainer: "output-area",
+        errorElement: "div",
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                email: true,
+                required: true,
+            },
+                message: {
+                    requried: true,
+                    maxlength: 2000
+                }
+            },
+        message: {
+            name: {
+                required: "Name is a required field",
+            },
+            email: {
+                email: "Please provide a valid address, Thank You!",
+                required: "Email is a requried field"
+            },
+            message: {
+                requried: true,
+                maxlength: 2000
+            }
+        }
+
+    })
+)
